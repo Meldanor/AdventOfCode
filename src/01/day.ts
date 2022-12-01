1610c2953da014f40a69dce9c7f5a7d53a4d85ab
Kilian Gärtner <kilian.gaertner@i22.de>
2022-12-01 21:27:25 +0100

    add: finish year 2022 day 1 part 2

--- src/01/day.ts
+++ src/01/day.ts
@@ -28,13 +28,19 @@ async function run(args: string[]): Promise<void> {
   );
   groups.all.push(groups.current);
 
-  const sums = groups.all.map((value: string[]) => {
-    return value.reduce((acc: number, current: string) => {
-      return acc + Number.parseInt(current);
-    }, 0);
-  });
-  const max = Math.max(...sums);
-  console.log(`The maximum an elf carries is ${max}`);
+  const sums = groups.all
+    .map((value: string[]) => {
+      return value.reduce((acc: number, current: string) => {
+        return acc + Number.parseInt(current);
+      }, 0);
+    })
+    .sort();
+
+  console.log(`The maximum an elf carries is ${sums.slice(-1)}`);
+  const sumTopThree = sums.slice(-3).reduce((acc: number, current: number) => {
+    return acc + current;
+  }, 0);
+  console.log(`The top three elves are carrying ${sumTopThree} of calories`);
 }
 
 export { run };
