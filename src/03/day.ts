121725e8e1e1632ff1e1283ce3f7478956a6ed11
Kilian Gärtner <kilian.gaertner@i22.de>
2022-12-03 10:54:04 +0100

    add: solve year 2022 day 3 part 2

--- src/03/day.ts
+++ src/03/day.ts
@@ -21,22 +21,44 @@ function range(size: number, startAt = 0) {
   return [...Array(size).keys()].map((i) => i + startAt);
 }
 
+function chunk<T>(array: Array<T>, chunkSize: number): Array<Array<T>> {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    const chunk = array.slice(i, i + chunkSize);
+    chunks.push(chunk);
+  }
+  return chunks;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function run(args: string[]): Promise<void> {
   const content = await readInput('src/03/input.txt');
+  // Part 1
   const sumOfPriorities = content.reduce(
     (acc: number, line: string): number => {
-      return acc + priorityOfSameItem(line);
+      return acc + priorityOfSameItemInRucksack(line);
     },
     0
   );
-  console.log(`(Part 1): The sum of the priorities are ${sumOfPriorities}`);
+  console.log(`(Part 1): The sum of the priorities is ${sumOfPriorities}`);
+
+  // Part 2
+  const groups = chunk(content, 3);
+  const sumOfBadgePriorities = groups.reduce(
+    (acc: number, line: string[]): number => {
+      return acc + priorityOfSameItemInGroup(line);
+    },
+    0
+  );
+  console.log(
+    `(Part 2): The sum of the priorities of each group is ${sumOfBadgePriorities}`
+  );
 }
 
-function priorityOfSameItem(rucksack: string): number {
+function priorityOfSameItemInRucksack(rucksack: string): number {
   const firstCompartment = rucksack.slice(0, rucksack.length / 2);
   const secondCompartment = rucksack.slice(rucksack.length / 2);
-  const sameItem = findSameItem(firstCompartment, secondCompartment);
+  const sameItem = findSameItemInRucksack(firstCompartment, secondCompartment);
   if (sameItem === '') {
     throw new Error('No same item found in ' + rucksack);
   } else {
@@ -47,14 +69,40 @@ function priorityOfSameItem(rucksack: string): number {
   return priorityValues[sameItem];
 }
 
-function findSameItem(
+function priorityOfSameItemInGroup(group: string[]): number {
+  const sameItem = findSameIteminGroup(group[0], group[1], group[2]);
+  if (sameItem === '') {
+    throw new Error('No same item found in ' + group);
+  } else {
+    // console.log(
+    //   `The same item for the rucksack ${firstCompartment} : ${secondCompartment} is '${sameItem}'`
+    // );
+  }
+  return priorityValues[sameItem];
+}
+
+function findSameItemInRucksack(
   firstCompartment: string,
   secondCompartment: string
 ): string {
   for (let i = 0; i < firstCompartment.length; i++) {
-    const element = firstCompartment[i];
-    if (secondCompartment.includes(element)) {
-      return element;
+    const item = firstCompartment[i];
+    if (secondCompartment.includes(item)) {
+      return item;
+    }
+  }
+  return '';
+}
+
+function findSameIteminGroup(
+  firstRucksack: string,
+  secondRucksack: string,
+  thirdRucksack: string
+): string {
+  for (let i = 0; i < firstRucksack.length; i++) {
+    const item = firstRucksack[i];
+    if (secondRucksack.includes(item) && thirdRucksack.includes(item)) {
+      return item;
     }
   }
   return '';
