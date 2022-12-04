d63ebfb4cd0cde63ae3cca60e13d9cb3d472e348
Kilian Gärtner <kilian.gaertner@i22.de>
2022-12-04 12:23:10 +0100

    add: solve year 2022 day 4 part 2

--- src/04/day.ts
+++ src/04/day.ts
@@ -1,4 +1,4 @@
-import { readInput } from '../utils.js';
+import { range, readInput } from '../utils.js';
 
 interface Range {
   start: number;
@@ -8,9 +8,10 @@ interface Range {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function run(args: string[]): Promise<void> {
   const content = await readInput('src/04/input.txt');
-  const overlappingAssignments = content.reduce(
+  // Part 1
+  const redundantAssignments = content.reduce(
     (acc: number, line: string): number => {
-      if (isOverlappingAssignment(line)) {
+      if (hasFullyContainedAssignment(line)) {
         return acc + 1;
       } else {
         return acc;
@@ -19,11 +20,25 @@ async function run(args: string[]): Promise<void> {
     0
   );
   console.log(
-    `(Part 1): There are '${overlappingAssignments}' overlaping assignments`
+    `(Part 1): There are '${redundantAssignments}' redudandant assignments`
+  );
+  // Part 2
+  const overlappingAssignments = content.reduce(
+    (acc: number, line: string): number => {
+      if (hasOverlappingAssignment(line)) {
+        return acc + 1;
+      } else {
+        return acc;
+      }
+    },
+    0
+  );
+  console.log(
+    `(Part 2): There are '${overlappingAssignments}' pairs that overlaps.`
   );
 }
 
-function isOverlappingAssignment(line: string): boolean {
+function hasFullyContainedAssignment(line: string): boolean {
   const pairs = line.split(',');
   const firstRange = parseRange(pairs[0]);
   const secondRange = parseRange(pairs[1]);
@@ -43,11 +58,44 @@ function parseRange(pair: string): Range {
 }
 
 function isRangeIncludedIn(thisRange: Range, otherRange: Range) {
-  // thisRange.start: 3
-  // thisRange.end: 7
-  // otherRange.start: 2
-  // otherRange.end: 8
   return thisRange.start >= otherRange.start && thisRange.end <= otherRange.end;
+}
+
+function hasOverlappingAssignment(line: string): boolean {
+  const pairs = line.split(',');
+  const firstRange = parseRange(pairs[0]);
+  const secondRange = parseRange(pairs[1]);
+
+  const firstRangeSet = rangeToSet(firstRange);
+  const secondRangeSet = rangeToSet(secondRange);
+
+  return (
+    isRangeSetOverlapping(firstRangeSet, secondRangeSet) ||
+    isRangeSetOverlapping(secondRangeSet, firstRangeSet)
+  );
+}
+
+function rangeToSet(givenRange: Range): Set<number> {
+  return new Set(
+    range(givenRange.end - givenRange.start + 1, givenRange.start)
+  );
+}
+
+function isRangeSetOverlapping(
+  thisRange: Set<number>,
+  otherRange: Set<number>
+) {
+  for (
+    let iterator = thisRange.values(), val = null;
+    (val = iterator.next().value);
+
+  ) {
+    if (otherRange.has(val)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 export { run };
