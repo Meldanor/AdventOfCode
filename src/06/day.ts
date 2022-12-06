ed5130a03788aeaa162165f5fe5987e29de70451
Kilian Gärtner <kilian.gaertner@i22.de>
2022-12-06 09:01:18 +0100

    add: solve year 2022 day 6 part 1 and 2

---
+++ src/06/day.ts
@@ -0,0 +1,41 @@
+import { readInput } from '../utils.js';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function run(args: string[]): Promise<void> {
+  const content = await readInput('src/06/input.txt');
+
+  console.log(
+    `(Part 1): The first start-of-packet marker is after '${findFirsStartOfPacketMarker(
+      content[0],
+      4
+    )}' characters.`
+  );
+  console.log(
+    `(Part 2): The first start-of-packet marker is after '${findFirsStartOfPacketMarker(
+      content[0],
+      14
+    )}' characters.`
+  );
+}
+
+function findFirsStartOfPacketMarker(
+  packet: string,
+  distinctChars: number
+): number {
+  const ringBuffer = [...packet.slice(0, distinctChars - 1)];
+
+  for (let i = 3; i < packet.length; i++) {
+    const element = packet[i];
+    ringBuffer.push(element);
+
+    if (new Set(ringBuffer).size === distinctChars) {
+      console.log(`First Start of Packet Marker: ${ringBuffer}`);
+      return i + 1;
+    }
+    ringBuffer.shift();
+  }
+
+  return -1;
+}
+
+export { run };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { run as day2 } from './02/day.js';
 import { run as day3 } from './03/day.js';
 import { run as day4 } from './04/day.js';
 import { run as day5 } from './05/day.js';
+import { run as day6 } from './06/day.js';
 
 const day = Number.parseInt(process.argv[2]);
 const args = process.argv.slice(3);
@@ -23,6 +24,9 @@ switch (day) {
     break;
   case 5:
     await day5(args);
+    break;
+  case 6:
+    await day6(args);
     break;
   default:
     console.warn('Not yet implemented');
